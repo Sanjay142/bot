1b0cac6b1850df6c1a82b2d0fa85f9044768f7f5
Sanjay142 <m.sanjay142005@gmail.com>
2025-03-08 23:07:39 +0530

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,33 +2,4 @@ import discord
 from discord.ext import commands
 
 
-TOKEN = 'YOUR_BOT_TOKEN'
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
-bot = commands.Bot(command_prefix='!', intents=intents)
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hello, {ctx.author.mention}!')
-
-@bot.command()
-async def echo(ctx, *, message):
-    await ctx.send(message)
-
-@bot.command()
-async def add(ctx, a: int, b: int):
-    await ctx.send(f'The sum is {a + b}')
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send("Command not found. Try !help for available commands.")
-    else:
-        await ctx.send("An error occurred.")
-
-bot.run(TOKEN)
